84680bc10f47a297f29139b9a9a29017930d73d6
KyRZy <krzysiekk09@o2.pl>
2017-12-14 14:05:00 +0100

    Skonfigurowanie referencji między użytkownikami, a drużynami

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,2 +1,6 @@
 class Team < ApplicationRecord
+    has_one :leader, :class_name => 'User'
+    has_many :users
+
+    validates :name, presence: true, uniqueness: {case_sensitive: false}
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  belongs_to :team
+  
+  validates :team, length: { minimum: 1 }, allow_nil: true
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
